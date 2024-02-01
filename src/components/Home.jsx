eca15a51b808321cb2c386e037d85d49574b9dd0
YoonJae Kim <yoonjaekim3@gmail.com>
2024-02-01 15:53:06 -0600

    Added Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,9 +2,11 @@ import { useLoadScript } from "@react-google-maps/api";
 
 export default function Home() {
     const {isLoaded} = useLoadScript({
-        
-    });
+        googleMapsApiKey: import.meta.env.VITE_REACT_APP_GOOGLE_MAPS_API_KEY,
+        libraries: ["places"],
 
+    });
+    if (!isLoaded) return <div>Loading...</div>
    return (
     <div>Map</div>
    ) 
