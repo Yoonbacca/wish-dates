38541e7e9f9a86edfbb09d8509f374202644e182
YoonJae Kim <yoonjaekim3@gmail.com>
2024-02-05 14:07:41 -0600

    Added chakra ui and react icons

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,50 +3,9 @@ import { GoogleMap, Marker,  DirectionsRenderer, Circle, MarkerClusterer } from 
 import Places from "./Places";
 import Distance from "./Distance";   
 
+
 export default function Map() {
    return (
     <div>Map</div>
    ) 
 }
-
-const defaultOptions = {
-   strokeOpacity: 0.5,
-   strokeWeight: 2,
-   clickable: false,
-   draggable: false,
-   editable: false,
-   visible: true,
- };
- const closeOptions = {
-   ...defaultOptions,
-   zIndex: 3,
-   fillOpacity: 0.05,
-   strokeColor: "#8BC34A",
-   fillColor: "#8BC34A",
- };
- const middleOptions = {
-   ...defaultOptions,
-   zIndex: 2,
-   fillOpacity: 0.05,
-   strokeColor: "#FBC02D",
-   fillColor: "#FBC02D",
- };
- const farOptions = {
-   ...defaultOptions,
-   zIndex: 1,
-   fillOpacity: 0.05,
-   strokeColor: "#FF5252",
-   fillColor: "#FF5252",
- };
- 
- const generateHouses = (position: google.maps.LatLngLiteral) => {
-   const _houses: Array<google.maps.LatLngLiteral> = [];
-   for (let i = 0; i < 100; i++) {
-     const direction = Math.random() < 0.5 ? -2 : 2;
-     _houses.push({
-       lat: position.lat + Math.random() / direction,
-       lng: position.lng + Math.random() / direction,
-     });
-   }
-   return _houses;
- };
