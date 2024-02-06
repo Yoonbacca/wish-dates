27ba7baa229edfcb19ea4ce58b14fb404beae418
YoonJae Kim <yoonjaekim3@gmail.com>
2024-02-06 14:25:13 -0600

    Added marker and google map comps

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,89 @@
-import { GoogleMap, MarkerF, useLoadScript } from "@react-google-maps/api";
-import { useState, useMemo } from 'react'
-import React from 'react'
-import './App.css'
+import { GoogleMap, MarkerF, useJsApiLoader } from "@react-google-maps/api";
+import { React, useState, useMemo, } from 'react'
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Flex,
+  HStack,
+  IconButton,
+  Input,
+  Text,
+} from '@chakra-ui/react'
+import { FaLocationArrow, FaTimes } from 'react-icons/fa'
+
+const center = {lat: 41.8781, lng: -87.6298}
 
 const App = () => {
-  const { isLoaded } = useLoadScript({
+  const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: import.meta.env.VITE_REACT_APP_GOOGLE_MAPS_API_KEY || "",
-  });
-  const center = useMemo(() => ({ lat: 41.8781, lng: -87.6298 }), []);
+  })
+
+  if (!isLoaded) {
+    return <div>Loading</div>
+  }
 
   return (
-    <div className="App">
-      {!isLoaded ? (
-        <h1>Loading...</h1>
-      ) : (
-        <GoogleMap
-          mapContainerClassName="map-container"
-          center={center}
-          zoom={10}
-        >
-          <MarkerF position={{ lat: 41.8781, lng: -87.6298 }} icon={"http://maps.google.com/mapfiles/ms/icons/green-dot.png"} />
+    <Flex
+      position='relative'
+      flexDirection='column'
+      alignItems='center'
+      bgColor='blue.200'
+      h='100vh'
+      w='100vw'
+    >
+      <Box position='absolute' left={0} top={0} h='100%' w='100%'>
+        {/* Google Map Box */}
+        <GoogleMap 
+          center={center} 
+          zoom={15} 
+          mapContainerStyle={{width: '100%', height: '100%'}}
+          options={{
+            zoomControl: false,
+            streetViewControl: false,
+            mapTypeControl: false,
+            fullscreenControl: false,
+          }}>
+            <MarkerF position={center} />
+          {/* Display markers or directions */}
         </GoogleMap>
-      )}
-    </div>
+      </Box>
+
+      <Box
+        p={4}
+        borderRadius='lg'
+        mt={4}
+        bgColor='white'
+        shadow='base'
+        minW='container.md'
+        zIndex='modal'
+      >
+        <HStack spacing={4}>
+          <Input type='text' placeholder='Origin' />
+          <Input type='text' placeholder='Destination' />
+          <ButtonGroup>
+            <Button colorScheme='pink' type='submit'>
+              Calculate Route
+            </Button>
+            <IconButton
+              aria-label='center back'
+              icon={<FaTimes />}
+              onClick={() => alert(123)}
+            />
+          </ButtonGroup>
+        </HStack>
+        <HStack spacing={4} mt={4} justifyContent='space-between'>
+          <Text>Distance: </Text>
+          <Text>Duration: </Text>
+          <IconButton
+            aria-label='center back'
+            icon={<FaLocationArrow />}
+            isRound
+            onClick={() => alert(123)}
+          />
+        </HStack>
+      </Box>
+    </Flex>
   );
 };
 
